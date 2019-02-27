f247bc4e0be4ceba2c8ba1ebfa46dcd14a112f3d
vyzo <vyzo@hackzen.org>
2019-02-27 10:07:02 +0200

    raw test server

---
+++ raw-test-server/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+var testFilePath string
+
+func main() {
+	listenAddr := flag.String("listen", ":4001", "server listen address")
+	testFile := flag.String("file", "data", "data file to serve")
+
+	flag.Parse()
+
+	if _, err := os.Stat(*testFile); err != nil {
+		log.Fatal(err)
+	}
+	testFilePath = *testFile
+
+	ln, err := net.Listen("tcp", *listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting conneciton: %s", err)
+			continue
+		}
+
+		go serve(c)
+	}
+}
+
+func serve(c net.Conn) {
+	defer c.Close()
+
+	log.Printf("Incomming connection from %s", c.RemoteAddr())
+
+	file, err := os.Open(testFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	start := time.Now().UnixNano()
+	n, err := io.Copy(c, file)
+	if err != nil {
+		log.Printf("Error transmiting file: %s", err)
+	}
+	end := time.Now().UnixNano()
+	log.Printf("Transmitted %d bytes in %s", n, time.Duration(end-start))
+}
